ee1e407d20940922dfcb146fcfa01305283f1a41
Sebastian Tramontana <sebastiantramontana@gmail.com>
2021-06-11 15:02:23 -0300

    FinishUnitOfWorkMiddleware fix

--- Backend/Core/Skyrmium.Api.Implementations/Errors/ErrorType.cs
+++ Backend/Core/Skyrmium.Api.Implementations/Errors/ErrorType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Skyrmium.Api.Implementations.Errors
+﻿namespace Skyrmium.Api.Implementations.Errors
 {
    public enum ErrorType
    {
--- Backend/Core/Skyrmium.Api.Implementations/Middlewares/FinishUnitOfWorkMiddleware.cs
+++ Backend/Core/Skyrmium.Api.Implementations/Middlewares/FinishUnitOfWorkMiddleware.cs
@@ -7,18 +7,19 @@ namespace Skyrmium.Api.Implementations.Middlewares
    public class FinishUnitOfWorkMiddleware
    {
       private readonly RequestDelegate _next;
-      private readonly IUnitOfWork _unitOfWork;
 
       public FinishUnitOfWorkMiddleware(RequestDelegate next)
       {
          _next = next;
-         //_unitOfWork = unitOfWork;
       }
-      public async Task Invoke(HttpContext context)
+      public async Task Invoke(HttpContext context, IUnitOfWork unitOfWork)
       {
          await _next.Invoke(context);
 
-         //await _unitOfWork.Finish();
+         if (HttpMethods.IsGet(context.Request.Method))
+            return;
+
+         await unitOfWork.Finish();
       }
    }
 }
